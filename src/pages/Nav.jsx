450a25f73ca73d0d0271b52152c2fe0057e4b47b
Kevin Perez <66193315+KevinPerez10@users.noreply.github.com>
2023-06-01 16:19:29 +0800

    Changes to Navbar

--- src/pages/Nav.jsx
+++ src/pages/Nav.jsx
@@ -2,13 +2,16 @@ import React from 'react'
 
 export default function Nav() {
     return (
-        <div className='bg-white flex justify-center w-full py-10'>
-            <div className='w-4/5 justify-between flex flex-row'>
-                <div className='w-1/2'>Logo</div>
-                <div className='w-1/2 flex flex-row justify-between'>
-                    <ul>Home</ul>
-                    <ul>About Us</ul>
-                    <ul>Contact Us</ul>
+        <div className='bg-white flex justify-center w-full py-5'>
+            <div className='w-full mx-10 border-2 flex flex-row'>
+                <ul>Logo</ul>
+                <div className='hidden ml-auto md:flex justify-end w-1/2'>
+                    <ul className='w-full text-center border-2 cursor-pointer bg-green-200 px-2'>Home</ul>
+                    <ul className='w-full text-center border-2 cursor-pointer bg-green-200 px-2'>About Us</ul>
+                    <ul className='w-full text-center border-2 cursor-pointer bg-green-200 px-2'>Contact Us</ul>
+                </div>
+                <div className='md:hidden ml-auto grid place-items-center text-2xl'>
+                    <ion-icon name="menu-outline"></ion-icon>
                 </div>
             </div>
         </div>
