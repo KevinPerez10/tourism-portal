e00d73a54bd185f673754dfb4d00dc58828fcc39
Kevin Perez <66193315+KevinPerez10@users.noreply.github.com>
2023-06-07 12:52:59 +0800

    Added AOS

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,9 @@
 import React from 'react'
 import Nav from './Nav'
+import AOS from 'aos'
+import 'aos/dist/aos.css'
+
+AOS.init()
 
 export default function Home() {
     return (
@@ -7,7 +11,12 @@ export default function Home() {
             <Nav/>
             <main className='flex relative items-center justify-center bg-home-image bg-cover bg-no-repeat h-screen w-full'>
                 <div className='absolute backdrop-brightness-50 h-full w-full'/>
-                <div className='flex flex-col text-white w-full text-4xl sm:text-5xl md:text-8xl text-center'>
+                <div
+                    data-aos='zoom-in'
+                    data-aos-delay="500"
+                    data-aos-duration="1000"
+                    className='flex flex-col text-white w-full text-4xl sm:text-5xl md:text-8xl text-center'
+                >
                     <h1 className='drop-shadow-lg'>Experience</h1>
                     <h2 className='puerto-header drop-shadow-lg font-bold'>Puerto Princesa</h2>
                     <p className='mt-5 text-lg drop-shadow-lg'>Where Nature's Masterpieces Come to Life</p>
@@ -19,7 +28,12 @@ export default function Home() {
                 </div>
             </main>
 
-            <section className='my-10 w-full bg-white z-10 gap-10 flex flex-col items-center justify-center'>
+            <section
+                data-aos='zoom-in-up'
+                data-aos-delay="100"
+                data-aos-duration="1000"
+                className='my-10 w-full bg-white z-10 gap-10 flex flex-col items-center justify-center'
+            >
                 <div className='flex flex-col md:w-1/2 w-4/5 sm:text-xl text-center'>
                     <h1 className='text-2xl font-bold'>Tropical Dreams Come True</h1>
                     <div>
@@ -31,27 +45,37 @@ export default function Home() {
                 </div>
             </section>
             
-            <section className='relative bg-start-image bg-cover bg-no-repeat flex flex-col gap-10 items-center w-full md:rounded-tr-[10rem] md:rounded-bl-[10rem]'>
+            <section
+                data-aos='fade-right'
+                data-aos-delay="100"
+                data-aos-duration="1000"
+                className='relative bg-start-image bg-cover bg-no-repeat flex flex-col gap-10 items-center w-full md:rounded-tr-[10rem] md:rounded-bl-[10rem]'
+            >
                 <div className='absolute backdrop-blur-sm backdrop-brightness-50 h-full w-full md:rounded-tr-[10rem] md:rounded-bl-[10rem]'/>
-                <div className='text-white lg:w-3/5 md:w-1/2 pt-10 w-4/5 md:self-start md:ml-10'>
+                <div className='text-white lg:w-3/5 md:w-1/2 pt-10 w-4/5 md:self-start lg:ml-40 md:ml-20'>
                     <h1 className='text-2xl font-bold drop-shadow-lg'>Start your journey now at Puerto Princesa!</h1>
                     <div className='drop-shadow-lg'>
                         Indulge in pure bliss as you soak up the sun on the pristine beaches of Puerto Princesa, Palawan, where paradise awaits.
                     </div>
                 </div>
                 <div className='grid place-items-center gap-10 w-4/5 grid-cols-4 drop-shadow-lg mb-20 text-white'>
-                    <div className='overflow-hidden relative cursor-pointer hover:bg-gray-900/25 font-bold rounded-xl grid place-items-center w-full h-36 sm:col-span-3 col-span-4'>
+                    <div data-aos='fade-right' className='overflow-hidden relative cursor-pointer hover:bg-gray-900/25 font-bold rounded-xl grid place-items-center w-full h-36 sm:col-span-3 col-span-4'>
                         <p className='drop-shadow-lg'>Underground River Tour</p>
                         <img className='absolute w-full -z-10 bottom-0' src="src/assets/images/lxonyeuu3jwtavzldwjp.webp" alt="" />
                     </div>
-                    <div className='border-2 overflow-hidden relative cursor-pointer hover:bg-gray-900/25 font-bold rounded-xl grid place-items-center w-full h-36 row-start-2 sm:col-span-2 col-span-4'>City Heritage Tour</div>
-                    <div className='border-2 overflow-hidden relative cursor-pointer hover:bg-gray-900/25 font-bold rounded-xl grid place-items-center w-full h-36 sm:col-span-2 col-span-4'>Island Hopping Tour</div>
-                    <div className='border-2 overflow-hidden relative cursor-pointer hover:bg-gray-900/25 font-bold rounded-xl grid place-items-center w-full h-36 col-span-4'>Package 4</div>
-                    <div className='border-2 overflow-hidden relative cursor-pointer hover:bg-gray-900/25 font-bold rounded-xl grid place-items-center w-full h-36 sm:col-start-2 sm:col-span-3 col-span-4'>Package 5</div>
+                    <div data-aos='fade-right' className='border-2 overflow-hidden relative cursor-pointer hover:bg-gray-900/25 font-bold rounded-xl grid place-items-center w-full h-36 row-start-2 sm:col-span-2 col-span-4'>City Heritage Tour</div>
+                    <div data-aos='fade-left' className='border-2 overflow-hidden relative cursor-pointer hover:bg-gray-900/25 font-bold rounded-xl grid place-items-center w-full h-36 sm:col-span-2 col-span-4'>Island Hopping Tour</div>
+                    <div data-aos='fade-right' className='border-2 overflow-hidden relative cursor-pointer hover:bg-gray-900/25 font-bold rounded-xl grid place-items-center w-full h-36 col-span-4'>Package 4</div>
+                    <div data-aos='fade-left' className='border-2 overflow-hidden relative cursor-pointer hover:bg-gray-900/25 font-bold rounded-xl grid place-items-center w-full h-36 sm:col-start-2 sm:col-span-3 col-span-4'>Package 5</div>
                 </div>
             </section>
 
-            <section className='bg-journey-image flex flex-col items-center justify-end text-white bg-cover bg-no-repeat md:mt-20 w-full relative'>
+            <section
+                data-aos='fade-up'
+                data-aos-delay="100"
+                data-aos-duration="1000"
+                className='bg-journey-image flex flex-col items-center justify-end text-white bg-cover bg-no-repeat md:mt-20 w-full relative'
+            >
                 <div className='absolute backdrop-brightness-50 h-full w-full'/>
                 <div class="custom-shape-divider-top-1686037844 hidden md:block">
                     <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
@@ -63,13 +87,14 @@ export default function Home() {
                     <p className='drop-shadow-lg w-[70%]'>Find Peace and Relaxation in Puerto Princesa, Palawan's Idyllic Retreats!</p>
                 </div>
             </section>
-            <footer className='w-full bg-[#222222] text-white flex py-40'>
+
+            <footer className='w-full bg-[#222222] text-white flex flex-col md:flex-row gap-5 py-40'>
                 <div className='w-full pl-10 py-20'>
                     <h1 className='text-4xl font-bold mb-10'>Get in touch with us!</h1>
                     <p>Torem ipsum dolor sit amet, consectetur adipiscing elit.</p>
                     <p>Torem ipsum dolor sit amet, consectetur adipiscing elit.</p>
                 </div>
-                <div className='w-full flex flex-col items-end pr-10 gap-5 py-20'>
+                <div className='w-full flex flex-col md:items-end pl-10 pr-10 gap-5 py-20'>
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero, iure?</p>
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero, iure?</p>
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero, iure?</p>
--- src/pages/Nav.jsx
+++ src/pages/Nav.jsx
@@ -2,15 +2,20 @@ import React from 'react'
 
 export default function Nav() {
     return (
-        <div className='bg-white border-2 z-20 fixed flex justify-center w-full'>
+        <div
+            data-aos='fade-down'
+            data-aos-delay="100"
+            data-aos-duration="1000"
+            className='bg-white z-20 fixed flex justify-center w-full'
+        >
             <div className='w-full xl:mx-40 md:mx-20 mx-5 text-xs flex flex-row items-center'>
                 <div>
                     <img className='md:w-20 w-10' src='src/assets/images/logo.png' alt="logo" />
                 </div>
                 <div className='hidden lg:text-lg ml-auto md:flex justify-end w-1/2'>
-                    <ul className='lg:w-full text-center cursor-pointer px-2'>Home</ul>
-                    <ul className='lg:w-full text-center cursor-pointer px-2'>About Us</ul>
-                    <ul className='lg:w-full text-center cursor-pointer px-2'>Contact Us</ul>
+                    <ul className='lg:w-full text-center cursor-pointer drop-shadow-xl px-2'>Home</ul>
+                    <ul className='lg:w-full text-center cursor-pointer drop-shadow-xl px-2'>About Us</ul>
+                    <ul className='lg:w-full text-center cursor-pointer drop-shadow-xl px-2'>Contact Us</ul>
                 </div>
                 <div className='md:hidden ml-auto grid place-items-center text-2xl'>
                     <ion-icon name="menu-outline"></ion-icon>
